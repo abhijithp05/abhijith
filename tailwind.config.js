2d6f774b4fd2da39758bd4386f29e6c1dade8279
admin <admin@admins-MacBook-Pro.local>
2025-01-10 13:02:00 +0530

    initial setup

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#1D4ED8",
+        secondary: "#6B7280",
+      },
+      spacing: {
+        128: "32rem",
+      },
+    },
+  },
+  plugins: [],
+};
